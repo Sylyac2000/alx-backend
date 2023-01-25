d913b8920f592ea39b1e785ce1a5fa31e61a3c8b
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-25 17:27:51 +0000

    1. FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" BasicCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache inherits from BaseCaching :
+      - is a caching system
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                fst_key = list(self.cache_data.keys())[0]
+                fst_item = self.cache_data.pop(list(self.cache_data.keys())[0])
+                print('DISCARD: ', fst_key)
+
+    def get(self, key):
+        """ return an item by key
+        """
+        if key is None or not (key in self.cache_data):
+            return None
+        return self.cache_data.get(key)
