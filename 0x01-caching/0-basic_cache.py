92c1ee8478bdbbd0e8acb37ab55b9d1bd279d843
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-25 12:36:58 +0000

    0. Basic dictionary upd

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ BasicCache module
 """
 
@@ -10,28 +10,15 @@ class BasicCache(BaseCaching):
       - is a caching system
     """
 
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        super().print_cache()
-        """print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))"""
-
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None or item is not None:
+        if not (key is None or item is None):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
+        """ return an item by key
         """
-        if key is None or key not in self.cache_data:
+        if key is None or not (key in self.cache_data):
             return None
         return self.cache_data.get(key)
