f9162703eb8fe79d1a1824fb4e5f28d0f7118b80
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-25 12:06:02 +0000

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache inherits from BaseCaching :
+      - is a caching system
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        super().print_cache()
+        """print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))"""
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,38 @@
+## 0x01. Caching
+
+## Requirements
+* Language: Javascript, Python 3.8.5 (and C )
+* OS: Ubuntu 20.04 LTS
+* Compiler: python3  (and gcc 9.3.0)
+* Version: MySQL  8.0.25
+* Style guidelines: [Pycodestyle] (https://github.com/PyCQA/pycodestyle)
+
+
+## Synopsis
+This repository holds some python projects I worked on at ALX SPECIALIZATION.
+
+# Requirements
+##General
+* What a caching system is
+* What FIFO means
+* What LIFO means
+* What LRU means
+* What MRU means
+* What LFU means
+* What the purpose of a caching system
+* What limits a caching system have
+
+
+## Tasks
+
+* 0. Basic dictionary
+* 1. FIFO caching
+* 2. LIFO Caching
+* 3. LRU Caching
+* 4. MRU Caching
+
+**Repo:**
+
+*   GitHub repository: `alx-backend`
+*   Directory: `0x01-caching`
+
