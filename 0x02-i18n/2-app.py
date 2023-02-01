f148d36c926c27984559352708525ae9cbc3c493
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-01 05:41:54 +0000

    2. Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,14 +16,13 @@ app = Flask(__name__)
 
 # Load babel
 app.config.from_object(Config)
+babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """ Get locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel = Babel(app)
 
 
 @app.route('/')
