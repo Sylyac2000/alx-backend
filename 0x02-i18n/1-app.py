651101d2b1e5ce98d68f8c0368d2359c446ee8a6
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-01 04:44:47 +0000

    1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module load flask app and adding Babel"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """config for babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+
+# Load babel
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ view function to load 1-index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0', debug=True)
